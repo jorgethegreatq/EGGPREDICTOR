9fc677a9a7830a5251baa55c4d2c0ad017404d69
jorgethegreatq <annzkecastillo456@gmail.com>
2025-07-22 14:00:34 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet('https://pastefy.app/JXeToacW/raw'))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+loadstring(game:HttpGet('https://pastefy.app/UC0yTb2b/raw'))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_P_E.lua')))()
